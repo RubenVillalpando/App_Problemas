aeead162c8b4fbf06933aa5f3f77bb5336abf001
Ruben Villalpando Bremont <a01376331@itesm.mx>
2021-09-06 20:09:13 -0500

    Initial commit

---
+++ app/src/main/java/com/example/problemas/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.problemas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.problemas.databinding.ActivityMainBinding
+import com.example.problemas.databinding.ProblemaUnoBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bindingUno: ProblemaUnoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        bindingUno = ProblemaUnoBinding.inflate(layoutInflater)
+
+        val vistaMain = binding.root
+        val vistaUno = bindingUno.root
+
+
+        setContentView(vistaUno)
+
+    }
+}
